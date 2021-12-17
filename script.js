bd22de528f689cbd3f047afc6732f9960aebc553
kuternega <90458770+kuternega@users.noreply.github.com>
2021-12-17 19:21:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).on('ready', function() {
+  $('.worked_with_us_slider1').slick({
+    infinite: true,
+     slidesToShow: 6,
+     slidesToScroll: 1,
+     autoplay: true,
+     autoplaySpeed: 2000,
+  });
+});
+
+$(document).on('ready', function() {
+  $('.worked_with_us_slider2').slick({
+    infinite: true,
+     slidesToShow: 6,
+     slidesToScroll: 1,
+     autoplay: true,
+     autoplaySpeed: 2000,
+  });
+});
