793e592c3b26747d9efb4fd3b8906e54713789ee
kuternega <90458770+kuternega@users.noreply.github.com>
2021-12-18 20:53:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 $(document).on('ready', function() {
   $('.worked_with_us_slider1').slick({
     infinite: true,
-     slidesToShow: 6,
-     slidesToScroll: 1,
-     autoplay: true,
-     autoplaySpeed: 2000,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    //autoplay: true,
+    autoplaySpeed: 2000,
   });
 });
 
 $(document).on('ready', function() {
   $('.worked_with_us_slider2').slick({
     infinite: true,
-     slidesToShow: 6,
-     slidesToScroll: 1,
-     autoplay: true,
-     autoplaySpeed: 2000,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    //autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
+
+
+$(document).on('ready', function() {
+  $('.rewiews_slider').slick({
+    infinite: true,
+    adaptiveHeight: true,
+    fade: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+
   });
 });
